b5f92f9df72a2d319d564f7515ce28b7a6030d8e
Darshan Mevada <darshan-mevada@mavenprofserv.com>
2024-11-14 17:51:14 +0530

    [backend implemented]

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+const cors = require('cors');
+// Use CORS middleware
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
@@ -13,16 +15,19 @@ var db = require('./db');
 db();
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- backend/routes/file/route.js
+++ backend/routes/file/route.js
@@ -1,15 +1,36 @@
-
+require('dotenv').config();
 var express = require('express');
 var router = express.Router();
 var upload = require('../../middlewares/multerServices');
+var File = require("./model")
 
 let imgUpload = upload("userProfile").fields([
     { name: "userProfile", maxCount: 1 },
 ]);
 
-router.post('/upload', imgUpload, (req, res) => {
-    res.send('Upload successful!');
-    console.log(req.file, req.body);
+router.post('/upload', imgUpload, async (req, res) => {
+    try {
+        console.log("Middleware working")
+        const { files } = req;
+
+        const userProfile = files.userProfile[0].filename;
+        console.log("🚀 userProfile:", userProfile)
+
+        const Uploadedfile = await File.create({ userProfile });
+        res.status(201).json({ result: "true", Uploadedfile });
+    } catch (error) {
+        res.status(500).json({ result: "false", message: "internal server error" });
+    }
+});
+
+router.get('/uploaded-img', async (req, res, next) => {
+    try {
+        const allData = await File.find({});
+        console.log("result >>>", allData);
+        res.status(200).json({ result: "true", images: allData });
+    } catch (error) {
+        res.status(500).json({ result: "false", message: "Internal server error" });
+    }
 });
 
 module.exports = router;
